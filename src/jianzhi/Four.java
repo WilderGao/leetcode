849f92a8bda0e5bc7f98023ed3c3e1ad480c857d
WilderGao <845956752@qq.com>
2019-02-02 16:40:49 +0800

    leetcode 22

--- src/jianzhi/Four.java
+++ src/jianzhi/Four.java
@@ -15,6 +15,7 @@ public class Four {
      * @return 是否有找到
      */
     private boolean findInPartiallySortedMatrix(int[][] matrix, int target) {
+        if (matrix == null) return false;
         //思路是每次都从右上角找起，如果比右上角的数字大则去掉对应的行，小则去掉对应的列
         //矩阵的长度
         int weight = matrix.length;
@@ -36,11 +37,13 @@ public class Four {
         return false;
     }
 
+    //其实同理也可以从左下角开始
+
     public static void main(String[] args) {
         int[][] a = new int[][]{
                 {1,2,8,9},
                 {2,4,9,12},
-                {4,6,10,13},
+                {4,7,10,13},
                 {6,8,10,15}
         };
         System.out.println(new Four().findInPartiallySortedMatrix(a, 7));
