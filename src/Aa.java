2cf3b0569b312e7474f36765468a6c555919dc84
WilderGao <845956752@qq.com>
2019-04-04 13:31:01 +0800

    a

---
+++ src/Aa.java
@@ -0,0 +1,14 @@
+/**
+ * @author WilderGao
+ * time 2019-03-24 13:55
+ * motto : everything is no in vain
+ * description
+ */
+public class Aa {
+    public static void main(String[] args) {
+        String s = "abc";
+	    int c = 333;
+        s = "bc";
+        s.intern();
+    }
+}
